ad930f686cc7a9cbdd78b8bcf5015713a7eca7a9
Lucas Magno <lmagno94@gmail.com>
2015-05-17 15:44:33 -0300

    Clean libmatriz

--- src/libmatriz.h
+++ src/libmatriz.h
@@ -1,10 +1,9 @@
 #ifndef libmatriz_h__
 #define libmatriz_h__
 
-extern char ** aloca_matriz(int m,int n);
-extern void libera_matriz(char **M,int n);
-extern char ** leia_mapa(char** M, int *m, int *n);
-extern void escreva_mapa_tela(char **M,int linhas,int colunas);
-extern void escrever_mapa_arquivo(char **M,int linhas,int colunas);
+extern char ** aloca_matriz(int m, int n);
+extern void libera_matriz(char **M, int m);
+extern int leia_matriz(char *nome, char** M, int *m, int *n);
+extern void escreva_matriz_tela(char **M, int m, int n);
 
 #endif
