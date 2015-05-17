b9a7bc277f0e08660b7cd14f8a412e1fcbfe659d
Lucas Magno <lmagno94@gmail.com>
2015-05-17 10:28:46 -0300

    Add header file for libmatriz

---
+++ src/libmatriz.h
@@ -0,0 +1,10 @@
+#ifndef libmatriz_h__
+#define libmatriz_h__
+
+extern char ** AlocaMatriz(int m,int n);
+extern void LiberaMatriz(char **M,int n);
+extern char ** leia_mapa(char** M);
+extern void escreva_mapa_tela(char **M,int linhas,int colunas);
+extern void escrever_mapa_arquivo(char **M,int linhas,int colunas);
+
+#endif
