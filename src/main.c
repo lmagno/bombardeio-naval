9aef7763c8015981dfb8d6ab6e53d84fb2f47779
Gustavo Covas <gtcovas@gmail.com>
2015-05-19 19:15:50 -0300

    Protótipo de src/main.c

--- src/main.c
+++ src/main.c
@@ -1,19 +1,29 @@
 #include <stdio.h>
 #include "libutils.h"
 #include "libmapa.h"
+#include "libembarcacoes.h"
+
+#define FALSE 0
+#define TRUE 1
 
 int main() {
     Mapa *mapa;
     char **M;
-    int m, i;
+    int x_barco, y_barco;
+    int fim_de_jogo = FALSE;
 
     mapa = leia_mapa_prompt();
+    /* Mas precisamos da string com o nome do arquivo pra poder chamar dispara_tiros*/
+
+    /*posiciona_barco();*/
+
     escreva_mapa_tela(mapa);
-    printf("%d %d\n", linhas(mapa), colunas(mapa));
-    escreva_matriz_tela(matriz(mapa), linhas(mapa), colunas(mapa));
-    M = matriz(mapa);
-    printf("começo!\n");
-    for (i = 0; i < linhas(mapa); i++) printf("%s\n", M[i]);
+
+    while(!fim_de_jogo){
+        rema_barco(mapa,x_barco,y_barco);        
+        fim_de_jogo = dispara_tiros(mapa, arquivo);
+    }
+
     libera_mapa(mapa);
     return 0;
 }
