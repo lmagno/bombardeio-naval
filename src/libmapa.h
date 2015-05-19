cb26be32d6a3ae57d155a287d1962bba2fba695d
Lucas Magno <lmagno94@gmail.com>
2015-05-19 19:44:53 -0300

    Altera leia_mapa_arquivo para receber uma string

--- src/libmapa.h
+++ src/libmapa.h
@@ -2,8 +2,7 @@
 #define libmapa_h__
 
 typedef struct MapaTag Mapa;
-extern Mapa* leia_mapa_prompt();
-extern Mapa* leia_mapa_arquivo(FILE *arq);
+extern int leia_mapa_arquivo(Mapa *mapa, char *nome);
 extern void escreva_mapa_tela(Mapa *mapa);
 extern int escreva_mapa_arquivo(char *nome, Mapa *mapa);
 extern void libera_mapa(Mapa *mapa);
