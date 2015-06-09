75d7133a0038e4ad2e64db5ecaf69dcd2a3144d4
tankerato <fernando.contato.direto@gmail.com>
2015-06-09 17:12:40 -0300

    coloqei um while com printf's como menu

--- src/main.c
+++ src/main.c
@@ -13,36 +13,53 @@ int main() {
     int x_barco, y_barco;
     int beco_3x,
         acertou_barco;
-
-    abre_mapa(&mapa);
-    seleciona_arquivo_saida(saida);
-    posiciona_barco(mapa, &x_barco, &y_barco);
-
-    escreva_mapa_tela(mapa);
-    escreva_mapa_arquivo(saida, mapa);
-
-    beco_3x = FALSE;
-    acertou_barco = FALSE;
+    char acao[1] ;
+    acao[0] = 'q';
+    Status *sts;
     while(1){
-        rema_barco(mapa, &x_barco, &y_barco, &beco_3x);
-        if (beco_3x) {
-            printf("Você perdeu o jogo. D:\n");
-            break;
-        }
+	do {
+            printf("        B O M B A R D E I R O   N A V A L        \n\n\n");
+	    printf("digite q para sair ou digite n para jogar:");
+            sts = read_str(acao);
+	}while(trata_status(sts));
+        if(acao[0] == 'n'){
 
-        dispara_tiros(mapa, saida, &acertou_barco);
-        if(acertou_barco) {
-            printf("Você perdeu o jogo. D:\n");
-            break;
-        }
+    	    abre_mapa(&mapa);
+    	    seleciona_arquivo_saida(saida);
+    	    posiciona_barco(mapa, &x_barco, &y_barco);
+
+    	    escreva_mapa_tela(mapa);
+    	    escreva_mapa_arquivo(saida, mapa);
+
+    	    beco_3x = FALSE;
+    	    acertou_barco = FALSE;
+    	    while(1){
+                rema_barco(mapa, &x_barco, &y_barco, &beco_3x);
+                if (beco_3x) {
+                    printf("Você perdeu o jogo. D:\n");
+                    break;
+                }
+
+                dispara_tiros(mapa, saida, &acertou_barco);
+                if(acertou_barco) {
+                    printf("Você perdeu o jogo. D:\n");
+                    break;
+                }
         
-        if (y_barco == linhas(mapa) - 1) {
-            printf("\nParabéns! Você ganhou! :D\n");
-            break;
-        }
-    }
+                if (y_barco == linhas(mapa) - 1) {
+                    printf("\nParabéns! Você ganhou! :D\n");
+                    break;
+                }
+            }
 
-    libera_mapa(mapa);
+            libera_mapa(mapa);
+        }
+        if(acao[0] == 'q')
+    	    break;
+        else
+	    printf("\n\nObviamente você não sabe escrever na língua portuguesa,\n\n tente novamente quando aprender\n\n\n\n\n\n\n\n\n\n");
+    }
+    
     return 0;
 }
 
