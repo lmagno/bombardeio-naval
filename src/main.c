c780ac591c654d8733d084ee475403e3fdef400f
Gustavo Covas <gtcovas@gmail.com>
2015-06-18 14:07:46 -0300

    Início da parte gráfica em src/main.c usando o xwc.

--- src/main.c
+++ src/main.c
@@ -1,65 +1,87 @@
 #include <stdio.h>
-#include "libxwc.h"
 #include "libembarcacoes.h"
-#include <unistd.h>
-#include <mcheck.h>
 
-#define FALSE 0
-#define TRUE 1
+// libs do xwc:
+#include "libxwc.h"
+#include <unistd.h>
+
+#undef Status                // Por quê?
+#include <mcheck.h>          // valgrind
+
+#define FALSE     0
+#define TRUE      1
+#define SPR_SIZE 32    // O tamanho de cada sprite;
 
 void abre_mapa(Mapa **mapa);
 void seleciona_arquivo_saida(char *saida);
 
-int main() {
+int main(int ac, char **av) {
+    // Isso tem a ver com o valgrind?
     mtrace();
+
+    // Variáveis da engine do jogo:
     Mapa *mapa;
     char saida[50];
     int x_barco, y_barco;
-    int beco_3x,
+    int beco_3x,             // "Booleanas" de passagem 
         acertou_barco;
-    char acao = 'q';
+    char acao = 'q';         
     Status *sts;
+
+    // Variáveis relacionadas ao xwc:
     WINDOW *w;
-    PIC inicio;
-    MASK msk;
-    w = InitGraph(400,400, "Bombardeiro Naval");
-    inicio = ReadPic(w, "tela_inicial.xpm", NULL);
-    msk = NewMask(inicio, 400, 400);
-    WFillRect(w,0,0,400,400,WNamedColor("NavyBlue"));
-    SetMask(inicio,msk);
-    PutPic(w, inicio, 0,0, 100, 100, 200, 0);
+    //PIC barco, Aux;
+    //MASK msk;
+    int window_w, window_h;
 
-
-    printf("        B O M B A R D E I R O   N A V A L        \n\n\n");
+    printf("        B O M B A R D E I O   N A V A L        \n\n\n");
     
     do {
-	    printf("digite s para sair ou digite j para jogar:");
+	    printf("Digite s para sair ou digite j para jogar: ");
         sts = read_char(&acao);
         
-    }while(trata_status(sts));
-    if(acao == 'j'){
+    } while (trata_status(sts));
+    
+    if (acao == 'j') {
 
         abre_mapa(&mapa);
         seleciona_arquivo_saida(saida);
+
+        // Define as dimensões da janela (baseadas no tamanho do mapa):
+        w_width = colunas(mapa) * SPR_SIZE;
+        w_height = linhas(mapa) * SPR_SIZE;
+
+        // xwc - abre a janela principal
+        w = InitGraph(w_width, w_height, "Bombardeio Naval");
+
+
     	posiciona_barco(mapa, &x_barco, &y_barco);
 
     	escreva_mapa_tela(mapa);
     	escreva_mapa_arquivo(saida, mapa);
 
-    	beco_3x = FALSE;
+    	beco_3x       = FALSE;
     	acertou_barco = FALSE;
-    	while(1){
+        
+    	while(TRUE) {
+            // A próxima remada:
             rema_barco(mapa, &x_barco, &y_barco, &beco_3x);
+            
             if (beco_3x) {
                 printf("Você perdeu o jogo. D:\n");
                 break;
             }
 
+            // Os tiros:
             dispara_tiros(mapa, saida, &acertou_barco);
-            if(acertou_barco) {
+
+            // O barco foi acertado?
+            if (acertou_barco) {
                 printf("Você perdeu o jogo. D:\n");
                 break;
             }
+           
+            // O jogador chegou ao final do tabuleiro?
             if (y_barco == linhas(mapa) - 1) {
                 printf("\nParabéns! Você ganhou! :D\n");
                 break;
@@ -67,12 +89,14 @@ int main() {
         }
 
         libera_mapa(mapa);
-    }
-    UnSetMask(inicio);
-    FreePic(inicio);
 
-    CloseGraph();
+        // xwc - Fecha a janela principal
+        CloseGraph();
+    }
+
+    // valgrind 
     muntrace();
+    
     return 0;
 }
 
@@ -84,10 +108,10 @@ void abre_mapa(Mapa **mapa) {
         do {
             printf("Entre com o nome do arquivo que contém o mapa: ");
             sts = read_str(entrada);
-        } while(trata_status(sts));
+        } while (trata_status(sts));
 
         sts = leia_mapa_arquivo(mapa, entrada);
-    } while(trata_status(sts));
+    } while (trata_status(sts));
 }
 
 void seleciona_arquivo_saida(char *saida) {
@@ -96,5 +120,5 @@ void seleciona_arquivo_saida(char *saida) {
     do {
         printf("Entre com o nome do arquivo de saída: ");
         sts = read_str(saida);
-    } while(trata_status(sts));
+    } while (trata_status(sts));
 }
